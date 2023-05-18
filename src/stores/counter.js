5d0f42c197d986f1a396c931769a8798a3684b25
yuladp1 <yuladp1@gmail.com>
2023-05-18 23:47:28 +0200

    changed path to img

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -12,7 +12,7 @@ export const useCounterStore = defineStore('counter',{
           "age": "5 років",
           "chip": "немає",
           "info": "Падре старший. Він дорослий і розумний. Падре живе в підвалі, до якого взимку 2022 чогось бігали люди. Прибігли такі вночі, їсти не дали, щось теревеніли.Чого без паштету приходити, неясно.",
-          "images": ['../src/assets/all-cats/padre-main.png', '../src/assets/all-cats/padre-1.png','../src/assets/all-cats/padre-2.png','../src/assets/all-cats/padre-3.png']
+          "images": ['../../public/all-cats/padre-main.png', '../src/assets/all-cats/padre-1.png','../src/assets/all-cats/padre-2.png','../src/assets/all-cats/padre-3.png']
       },
       {
         "ID": "Padre",
