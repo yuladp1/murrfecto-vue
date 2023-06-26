bfaf1cfdca815f6c5c1dce6bd37ab0228fd60e20
yuladp1 <yuladp1@gmail.com>
2023-06-26 20:38:56 +0200

    change DonateModal

--- src/components/DonateModal.vue
+++ src/components/DonateModal.vue
@@ -5,28 +5,28 @@
     <div class="donates-wrapper">
       <p>Зібрані кошти підуть на харчування та медичну допомогу</p>
       <div class="tabs">
-        <button :class="{ active: isActiveTab === 1 }" @click="this.addActiveTab(1)">
+        <button :class="{ active: isActiveTab === 1 }" @click="addActiveTab(1)">
           Одноразово
         </button>
-        <button :class="{ active: isActiveTab === 2 }" @click="this.addActiveTab(2)">
+        <button :class="{ active: isActiveTab === 2 }" @click="addActiveTab(2)">
           Щомісячно
         </button>
       </div>
       <div class="tabcontent">
-        <button class="sum20" @click="this.sum = 20">20</button>
-        <button class="sum50" @click="this.sum = 50">50</button>
-        <button class="sum100" @click="this.sum = 100">100</button>
-        <button class="sum200" @click="this.sum = 200">200</button>
-        <input class="other" v-model="this.sum" :placeholder="this.sum" />
+        <button class="sum20" @click="sum = 20">20</button>
+        <button class="sum50" @click="sum = 50">50</button>
+        <button class="sum100" @click="sum = 100">100</button>
+        <button class="sum200" @click="sum = 200">200</button>
+        <input class="other" v-model="sum" :placeholder="sum" />
       </div>
       <p>Допомогти конкретному котику</p>
       <select v-model="selectedCat">
         <option selected>Оберіть пухнастика</option>
-        <option v-for="cat in base.catsArray" :key="cat.ID" :value="cat.value">
+        <option v-for="cat in base.catsArray" :key="cat.ID" :value="catSelect">
           {{ cat.CatsName }}
         </option>
       </select>
-      <button class="nav__button" onclick="this.createOrder(this.sum,cat.value)">Допомогти</button>
+      <button class="nav__button" onclick="this.createOrder(sum.value, catSelect.value)">Допомогти</button>
     </div>
   </section>
   <Teleport />
@@ -64,11 +64,11 @@ export default {
       button.setResponseUrl('http://example.com/result/')
       button.setHost('pay.fondy.eu')
       button.addField({
-        label: 'Допомога для {cat.value}',
+        label: `Допомога для ${this.cat.value}`,
         name: 'order_desc',
         value: order_desc
       })
-      location.href = button.getUrl()
+      window.location.href = button.getUrl()
     }
   }
 }
